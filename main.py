a1e43c8b3b82881bf1ddbf2fccf92e2be5eb3b36
fabiopereiraz <fabio.pereira@icarros.com.br>
2024-08-06 06:17:05 -0300

    ajustes

--- main.py
+++ main.py
@@ -1,120 +1,73 @@
-from threading import Lock
-from flask import Flask, render_template, session, request, \
-    copy_current_request_context
-from flask_socketio import SocketIO, emit, join_room, leave_room, \
-    close_room, rooms, disconnect
-
-# Set this variable to "threading", "eventlet" or "gevent" to test the
-# different async modes, or leave it set to None for the application to choose
-# the best option based on installed packages.
-async_mode = None
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import json
+import time
+import threading
+import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode=async_mode)
-thread = None
-thread_lock = Lock()
+socketio = SocketIO(app)
 
+def fetch_bitcoin_price():
+    response = requests.get('https://api.coinbase.com/v2/prices/btc-usd/spot')
+    data = response.json()
+    return data['data']['amount']
 
-def background_thread():
-    """Example of how to send server generated events to clients."""
-    count = 0
+def send_message():
     while True:
-        socketio.sleep(10)
-        count += 1
-        socketio.emit('my_response',
-                      {'data': 'Server generated event', 'count': count})
-
+        price = fetch_bitcoin_price()
+        message = {
+            "e": "trade",
+            "E": int(time.time() * 1000),
+            "s": "BTCUSD",
+            "p": price
+        }
+        socketio.emit('bitcoin_price', json.dumps(message), namespace='/ws')
+        #socketio.emit('bitcoin_price', json.dumps(message))
+        #print("Message sent:", message)
+        time.sleep(2)
 
 @app.route('/')
 def index():
-    return render_template('index.html', async_mode=socketio.async_mode)
+    return "WebSocket server is running!"
 
+@app.route('/opa')
+def index_opa():
+    return render_template('index.html')
 
-@socketio.event
-def my_event(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
+@app.route('/sessions')
+def index_sessions():
+    return render_template('sessions.html')
 
+@socketio.on('connect', namespace='/ws')
+def handle_connect():
+    print('Client connected')
+    emit('bitcoin_price', json.dumps({"message": "Connected to WebSocket"}))
 
-@socketio.event
-def my_broadcast_event(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
-
-
-@socketio.event
-def join(message):
-    join_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
-
-
-@socketio.event
-def leave(message):
-    leave_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
-
-
-@socketio.on('close_room')
-def on_close_room(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
-                         'count': session['receive_count']},
-         to=message['room'])
-    close_room(message['room'])
-
-
-@socketio.event
-def my_room_event(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         to=message['room'])
-
-
-@socketio.event
-def disconnect_request():
-    @copy_current_request_context
-    def can_disconnect():
-        disconnect()
-
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    # for this emit we use a callback function
-    # when the callback function is invoked we know that the message has been
-    # received and it is safe to disconnect
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']},
-         callback=can_disconnect)
-
-
-@socketio.event
-def my_ping():
-    emit('my_pong')
-
-
-@socketio.event
-def connect():
-    global thread
-    with thread_lock:
-        if thread is None:
-            thread = socketio.start_background_task(background_thread)
-    emit('my_response', {'data': 'Connected', 'count': 0})
-
-
-@socketio.on('disconnect')
-def test_disconnect():
-    print('Client disconnected', request.sid)
-
+@socketio.on('disconnect', namespace='/ws')
+def handle_disconnect():
+    print('Client disconnected')
+    
+# Receive the test request from client and send back a test response
+@socketio.on('test_message', namespace='/ws')
+def handle_message(data):
+    print('received message: ' + str(data))
+    emit('test_response', {'data': 'Test response sent'})
+    
+#socketio.on_namespace(MyNamespace('/'))
 
 if __name__ == '__main__':
-    socketio.run(app, allow_unsafe_werkzeug=True)
-
+    print('Servidor Websocket Python 1.0 - rodando...')
+    print('==Socket.io==')
+    print('port:5011')
+    
+    # BITCOIN Start the message sending thread
+    # thread = threading.Thread(target=send_message)
+    # thread.daemon = True
+    # thread.start()
+    
+    # Run the Flask app
+    #socketio.run(app, host='0.0.0.0', port=5011)
+   # socketio.run(app, host='0.0.0.0', port=5011, debug=True)
+    socketio.run(app, debug=True)
